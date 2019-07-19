2e5391838c16ea26a23aa0416c7677e32ec15346
Avisrur <nadavavisrur@gmail.com>
2019-07-19 16:47:58 +0300

    added a simple connection to mongo and one post api

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
-
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 const app = express();
+var db;
+var url = 'mongodb://localhost:27017/PetStory';
+app.use(bodyParser.urlencoded({ extended: true }))
 
+MongoClient.connect(url,{ useNewUrlParser: true }, (err, database) => {
+    if (err) return console.log(err)
+    db = database.db('PetStory');
+    app.listen(3000, () => {
+        console.log('listening on 3000')
+    })
+})
 
-app.get('/', function(req, res) {
+app.post('/quotes', (req, res) => {
+    db.collection('quotes').save(req.body, (err, result) => {
+      if (err) return console.log(err)
+  
+      console.log('saved to database')
+      res.redirect('/')
+    })
+  })
+
+app.get('/', function (req, res) {
     res.send('Hello World')
 });
-
-app.listen(3000, function() {
-    console.log('listening on 3000')
-});
-
