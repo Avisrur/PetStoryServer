cbed9251ca92e1f670d5c5f709e639943e211e74
Avisrur <nadavavisrur@gmail.com>
2019-08-11 15:25:59 +0300

    added socket io and fixed post

--- server.js
+++ server.js
@@ -2,15 +2,24 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const errorHandler = require('./helpers/error-handler');
-// const http = require('http').Server(app);
-// const io = require('socket.io')(http);  for trying to make feed real time
-
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 app.use(cors());
+const errorHandler = require('./helpers/error-handler');
 
-// global error handler
+
+const serverIO = require('http').Server(app);
+const io = require('socket.io')(serverIO);
+const postService = require('./services/post/post-service');
+// io.origins('localhost:4200 http://localhost:4200 https://localhost:4200');
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+
+  socket.on('new-post', (post) => {
+    postService.create(post);
+  });
+});
 
 
 app.use('/users', require('./services/user/user-controller'));
@@ -19,6 +28,10 @@ app.use('/parks', require('./services/park/park-controller'));
 app.use('/posts', require('./services/post/post-controller'));
 
 app.use(errorHandler);
+
+serverIO.listen(3001,  () => {
+  console.log('serverIO listenining on port 3001')
+});
 
 const server = app.listen(3000, function () {
   console.log('Server listening on port ' + 3000);
--- services/post/post-service.js
+++ services/post/post-service.js
@@ -10,7 +10,7 @@ module.exports = {
 };
 
 async function getAll() {
-    return await Post.find();
+    return await Post.find().sort('-timestamp');
 }
 
 async function getById(id) {
